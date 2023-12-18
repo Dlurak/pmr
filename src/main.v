2c58479c577287322107e5d6988bcf98ae71037f
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-12-18 23:05:31 +0100

    Migreate from flags to cli

--- src/main.v
+++ src/main.v
@@ -1,31 +1,41 @@
 module main
 
 import package_manager
-import flag
 import os
+import cli
 
 fn main() {
-	mut fp := flag.new_flag_parser(os.args)
-	fp.application('pmr')
-	fp.version('v0.0.1')
-	fp.description('PMR - Your unified package manager solution')
-	fp.skip_executable()
+	default_manager := package_manager.get_manager_name() or { '' }
 
-	default_manager_name := package_manager.get_manager_name() or { 'default' }
-
-	manager_name := fp.string('package-manager', 0, default_manager_name, 'Specify a package manager to use, defaults to the currently used one.')
-
-	help := fp.bool('help', `h`, false, 'Show the help')
-	show_current_manager := fp.bool('show-current', 0, false, "See which package manager is beeing used, if you don'nt specify one it will be the detected one")
-
-	if help {
-		println(fp.usage())
-		exit(0)
+	mut app := cli.Command{
+		name: 'pmr'
+		description: 'PMR is way to simplify the work with js package managers'
+		commands: [
+			cli.Command{
+				name: 'show-current'
+				description: 'Output the currently active package manager'
+				execute: fn (cmd cli.Command) ! {
+					manager := cmd.flags.get_string('package-manager') or {
+						eprintln('Something went wrong')
+						exit(1)
+					}
+					println(manager)
+					return
+				}
+			},
+		]
+		flags: [
+			cli.Flag{
+				flag: .string
+				name: 'package-manager'
+				abbrev: 'pm'
+				global: true
+				required: false
+				default_value: [default_manager]
+			},
+		]
 	}
 
-	manager := package_manager.PackageManager.new(manager_name)
-
-	if show_current_manager {
-		println(manager.str())
-	}
+	app.setup()
+	app.parse(os.args)
 }
